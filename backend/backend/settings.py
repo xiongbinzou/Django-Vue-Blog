2b01354a3f3817795c304c48995ddb94230d7542
xiongbinzou <xiongbinzou@163.com>
2023-04-03 20:43:32 +0800

    [feat] 添加容器化部署文件

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,6 +86,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'my_blog',
+#         'USER': 'root',
+#         'PASSWORD': 'mypassword',
+#         'HOST': 'db',
+#         'PORT': 3306,
+#         'OPTIONS': {'charset': 'utf8mb4'}
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
